e190782c270b925d0e7ff6c85c899b4e2ae8834c
Ivan Boyanov <Theflowzon3@gmail.com>
2023-06-27 18:01:29 +0300

    react init

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div>
+            <ul>
+                <li>About Me</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
